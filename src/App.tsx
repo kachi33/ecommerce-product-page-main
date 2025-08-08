3c3e4d5049ff8f6d7402c2ce8157d2789c88897e
coCo🙊 <72194813+kachi33@users.noreply.github.com>
2025-08-08 23:25:32 +0100

    feat: Project setup✨

---
+++ src/App.tsx
@@ -0,0 +1,29 @@
+function App() {
+  return (
+    <>
+      <div className="bg-white text-black min-h-screen">
+        <div className="container mx-auto px-4">
+          <h1 className="text-3xl font-bold my-4">E-commerce Product Page</h1>
+          <p className="text-lg">Welcome to the product page!</p>
+         <p> Collections Men Women About Contact Sneaker Company Fall Limited
+          Edition Sneakers These low-profile sneakers are your perfect casual
+          wear companion. Featuring a durable rubber outer sole, they'll
+          withstand everything the weather can offer. $125.00 50% $250.00 0 Add
+          to cart</p>
+          <div className="attribution">
+            Challenge by{" "}
+            <a
+              href="https://www.frontendmentor.io?ref=challenge"
+              target="_blank"
+            >
+              Frontend Mentor
+            </a>
+            . Coded by <a href="#">Your Name Here</a>.
+          </div>
+        </div>
+      </div>
+    </>
+  );
+}
+
+export default App;
